618493c7ffcbccecf118841c1f91499e17dee03c
Gjøvåg <christopher.gjovag@knowit.no>
2019-11-01 09:27:24 +0100

    First commit for Vue

---
+++ webpage/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <rating-button v-for="i in [-1, 0, 1]" :key="i" :btnType="i" v-on:rating-click="ratingClick" class="btn" ></rating-button>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import RatingButton from './components/RatingButton.vue'
+
+export default {
+  name: 'app',
+  components: {
+    RatingButton
+  },
+
+  methods: {
+    ratingClick: (rating) => {
+      alert(`You pressed ${rating}`)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.btn {
+  display: inline-block;
+  size: 42px;
+}
+
+</style>
---
+++ webpage/src/components/RatingButton.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="div">
+    <button id="button" :style="style" onClick="click">
+      {{ btnText }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    btnType: {
+      validator: function (value) {
+        return [-1, 0, 1].indexOf(value) !== -1
+      }
+    }
+  },
+
+  computed: {
+    style: function() {
+      if (this.btnType === -1) {
+        return 'background-color: red'
+      }
+      else if (this.btnType === 0) {
+        return 'background-color: yellow'
+      }
+      else if (this.btnType === 1) {
+        return 'background-color: green'
+      }
+      else {
+        return ''
+      }
+    },
+    btnText: function() {
+      if (this.btnType === -1) {
+        return ':('
+      }
+      else if (this.btnType === 0) {
+        return ':|'
+      }
+      else if (this.btnType === 1) {
+        return ':)'
+      }
+      else {
+        return ''
+      }
+    }
+  },
+  methods: {
+    click: () => {
+      // eslint-disable-next-line
+      console.log(`Clicked ${this.btnType}`)
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+#button {
+  font-size: 18pt;
+  padding: 15px 32px;
+}
+
+</style>

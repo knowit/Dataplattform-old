8335f2ffabe432632dc9c6eeb8e43528d46cdaaf
katrinewi <katrinew0702@gmail.com>
2019-11-01 15:48:01 +0100

    Almost working input field of front page

---
+++ webpage/src/components/CodeInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="container">
+    <input
+      class="input-field"
+      maxlength="1"
+      v-on:keyup="back($event,n)"
+      v-on:input="next($event, n)"
+      v-model="text[n]"
+      ref="input"
+      v-for="n in 5"
+      :key="n"
+      v-on:focus="$event.target.select()"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: []
+    };
+  },
+
+  methods: {
+    next(event, n) {
+      if (this.text[n] && this.text[n] !== " " && n < 5) {
+        this.$refs.input[n].focus();
+      }
+    },
+    back(event, n) {
+      if (
+        event.key === "Backspace" &&
+        (!this.text[n] || this.text[n] === "") &&
+        n > 1
+      ) {
+        this.$refs.input[n - 2].focus();
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.input-field {
+  font-size: 25px;
+  padding: 11px 45px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 2px solid #e3e3e3;
+  border-radius: 8px;
+  opacity: 1;
+  text-align: center;
+  font: Regular 28px/32px Arial;
+  letter-spacing: 0;
+  color: #707070;
+  width: 1em;
+}
+</style>
---
+++ webpage/src/components/StartPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="div">
+    <img src="../assets/app-logo.svg" />
+    <div id="input">
+      <code-input></code-input>
+      <button id="ok-button">OK</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import CodeInput from "./CodeInput.vue";
+
+export default {
+  components: {
+    CodeInput
+  },
+  data() {
+    return {
+      code: ""
+    };
+  },
+  methods: {
+    reverseCode: function() {
+      this.code = "hehe";
+    }
+  }
+};
+</script>
+
+<style scoped>
+#input {
+  margin-top: 22px;
+}
+#input-field:focus {
+  outline: 0;
+}
+
+#ok-button:focus {
+  outline: 0;
+}
+
+#ok-button {
+  font-size: 25px;
+  padding: 11px 13px;
+  background: #e3e3e3 0% 0% no-repeat padding-box;
+  border: 2px solid #e3e3e3;
+  border-radius: 8px;
+  opacity: 1;
+  text-align: center;
+  font: Regular 28px/32px Arial;
+  letter-spacing: 0;
+  color: #afafaf;
+  margin-left: 8px;
+}
+</style>

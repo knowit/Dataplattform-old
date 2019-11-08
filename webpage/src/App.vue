097a593242448139af11f0b38ca9b430ee1a4dc4
katrinewi <katrinew0702@gmail.com>
2019-11-08 11:19:43 +0100

    Deleted unused code

--- webpage/src/App.vue
+++ webpage/src/App.vue
@@ -1,27 +1,15 @@
 <template>
   <div id="app">
-    <!--<img alt="Vue logo" src="./assets/logo.png">-->
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <!-- <rating-button
-      v-for="i in [-1, 0, 1]"
-      :key="i"
-      :btnType="i"
-      v-on:rating-click="ratingClick"
-      class="btn"
-    ></rating-button>-->
     <start-page></start-page>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-//import RatingButton from "./components/RatingButton.vue";
 import StartPage from "./components/StartPage.vue";
 
 export default {
   name: "app",
   components: {
-    //RatingButton,
     StartPage
   },
 

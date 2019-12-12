9fe8d56b7976717a70836093285b1842ab5aa444
erlendgje <erlendgje@gmail.com>
2019-12-12 08:20:36 +0100

    Added SnappySense to Aurora

--- services/structured_mysql/batch_job_aurora/batch_job_aurora.py
+++ services/structured_mysql/batch_job_aurora/batch_job_aurora.py
@@ -41,6 +41,7 @@ DEFAULT_TYPES = [
     "LinkedInDailyFollowerStatsType",
     "LinkedInDailyShareStatsType",
     "LinkedInDailyPageStatsType",
+    "SnappySenseType",
 ]
 
 # Assume running hourly by default. Request the last 1h10m of data.
---
+++ services/structured_mysql/batch_job_aurora/data_types/SnappySenseType.py
@@ -0,0 +1,12 @@
+from data_types.AbstractType import AbstractType
+
+
+class SnappySenseType(AbstractType):
+	attributes_keep = {
+		("location", str): ["data", "location"],
+		("co2", int): ["data", "co2"],
+		("light", int): ["data", "light"],
+		("temperature", float): ["data", "temperature"],
+		("humidity", float): ["data", "humidity"],
+		("movement", bool): ["data", "movement"],
+	}

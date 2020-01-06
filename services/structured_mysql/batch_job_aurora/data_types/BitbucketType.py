7a802d75f9b31effe82669d07ec34d171b57d2f3
Johannes Berg <johannes.berg@knowit.no>
2020-01-06 14:38:31 +0100

    update Bitbucket type

--- services/structured_mysql/batch_job_aurora/data_types/BitbucketType.py
+++ services/structured_mysql/batch_job_aurora/data_types/BitbucketType.py
@@ -3,11 +3,11 @@ from data_types.AbstractType import AbstractType
 
 class BitbucketType(AbstractType):
     attributes_keep = {
-        ("id", str): ["id"],
-        ("displayId", str): ["displayId"],
-        ("authorTimestamp", int): ["authorTimestamp"],
-        ("committerTimestamp", int): ["committerTimestamp"],
-        ("message", str): ["message"],
-        ("parentCommitId", str): ["parents", "id"],
-        ("parentCommitDisplayId", str): ["parents", "displayId"],
-        ("repositoryId", int): ["repo", "id"]}
+        ("id", str): ["data", "id"],
+        ("displayId", str): ["data", "displayId"],
+        ("authorTimestamp", int): ["data", "authorTimestamp"],
+        ("committerTimestamp", int): ["data", "committerTimestamp"],
+        ("message", str): ["data", "message"],
+        ("parentCommitId", str): ["data", "parents", "id"],
+        ("parentCommitDisplayId", str): ["data", "parents", "displayId"],
+        ("repositoryId", int): ["data", "repo", "id"]}

2189b4e7cb86a019b3c75ffeb671e0cb0727bd9a
Erlend Gjelsvik <erlend@pop-os.localdomain>
2020-01-08 15:07:48 +0100

    Added noise data to SnappySense

--- services/structured_mysql/batch_job_aurora/data_types/SnappySenseType.py
+++ services/structured_mysql/batch_job_aurora/data_types/SnappySenseType.py
@@ -5,6 +5,7 @@ class SnappySenseType(AbstractType):
 	attributes_keep = {
 		("location", str): ["data", "location"],
 		("co2", int): ["data", "co2"],
+		("noise", float): ["data", "noise"],
 		("light", int): ["data", "light"],
 		("temperature", float): ["data", "temperature"],
 		("humidity", float): ["data", "humidity"],

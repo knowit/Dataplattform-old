2236dd35f7bd1fd32704b9bfb53199b18fb7346d
Kim Hovden Duong <kimhduo@gmail.com>
2020-01-09 16:55:27 +0100

    Changed SQL queries to use SQLAlchemy ORM

--- services/structured_mysql/GoogleFormsBatch.py
+++ services/structured_mysql/GoogleFormsBatch.py
@@ -5,9 +5,28 @@ import os
 from batch_job_aurora.batch_job_aurora import format_url, fetch_data_url
 from datetime import datetime as dt
 from dotenv import load_dotenv
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(dotenv_path=os.path.join(BASEDIR, '.env'))
+
+db_host = os.getenv("DATAPLATTFORM_AURORA_HOST")
+db_port = os.getenv("DATAPLATTFORM_AURORA_PORT")
+db_user = os.getenv("DATAPLATTFORM_AURORA_USER")
+db_password = os.getenv("DATAPLATTFORM_AURORA_PASSWORD")
+db_db = os.getenv("DATAPLATTFORM_AURORA_DB_NAME")
+
+Base = automap_base()
+engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_db}?charset=utf8mb4')
+Base.prepare(engine, reflect=True)
+
+Form = Base.classes.GoogleFormsType
+Question = Base.classes.GoogleFormsQuestionType
+Answer = Base.classes.GoogleFormsAnswerType
+
+session = Session(engine)
 
 DEFAULT_TIMESTAMP_TO = timestamp = int(dt.now().timestamp())
 DEFAULT_TIMESTAMP_FROM = 1478399468
@@ -16,15 +35,6 @@ def main():
     base_url = os.getenv("DATAPLATTFORM_FETCH_URL")
     url = format_url(base_url, 'GoogleFormsType', DEFAULT_TIMESTAMP_FROM, DEFAULT_TIMESTAMP_TO)
     docs = fetch_data_url(url)
-    
-    connection = pymysql.connect(
-    host=os.getenv("DATAPLATTFORM_AURORA_HOST"),
-    port=int(os.getenv("DATAPLATTFORM_AURORA_PORT")),
-    user=os.getenv("DATAPLATTFORM_AURORA_USER"),
-    password=os.getenv("DATAPLATTFORM_AURORA_PASSWORD"),
-    db=os.getenv("DATAPLATTFORM_AURORA_DB_NAME"),
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor)
 
     forms = {}
     questions = []
@@ -52,47 +62,42 @@ def main():
         })
 
     try: 
-        with connection.cursor() as cursor:
-            for key in list(forms):
-                cursor.execute(f"SELECT id FROM GoogleFormsType WHERE id = '{key}'")
-                exists = cursor.fetchone()
-                if exists is None:
-                    cursor.execute(f"INSERT INTO GoogleFormsType VALUES ('{key}', %s, %s)", (forms[key]['title'], forms[key]['description']))
-            for key in range(len(questions)):
-                cursor.execute(f"SELECT unique_id, text_question, type, version FROM GoogleFormsQuestionType WHERE form_question_id = '{questions[key]['form_question_id']}' \
-                    AND form_id = '{questions[key]['form_id']}' ORDER BY version DESC")
-                result = cursor.fetchone()
-                if result is None:
-                    questions[key]['version'] = 1
-                    cursor.execute(f"INSERT INTO GoogleFormsQuestionType VALUES ('{questions[key]['unique_id']}', '{questions[key]['form_id']}', \
-                        %s, '{questions[key]['question_type']}', {questions[key]['version']}, {questions[key]['form_question_id']})", (questions[key]['text_question']))
-                    connection.commit()
-                    continue
-                if (result['text_question'] != questions[key]['text_question'] or result['type'] != questions[key]['question_type']):
-                    questions[key]['version'] = result['version'] + 1
-                    cursor.execute(f"INSERT INTO GoogleFormsQuestionType VALUES ('{questions[key]['unique_id']}', '{questions[key]['form_id']}', \
-                        %s, '{questions[key]['question_type']}', {questions[key]['version']}, {questions[key]['form_question_id']})", (questions[key]['text_question']))
-                    connection.commit()
-                else:
-                    for answer in answers:
-                        answer.update((k, result['unique_id']) for k, v in answer.items() if v == questions[key]['unique_id'])
+        for key in list(forms):
+            result = session.query(Form).filter(Form.id == key).first()
+            if result is None:
+                session.add(Form(id=key, title=forms[key]['title'], description=forms[key]['description']))
+        for key in range(len(questions)):
+            result = session.query(Question).filter(Question.form_question_id == questions[key]['form_question_id'], \
+                Question.form_id == questions[key]['form_id']).order_by(Question.version.desc()).first()
+            if result is None:
+                session.add(Question(unique_id = questions[key]['unique_id'], form_id = questions[key]['form_id'], \
+                    text_question=questions[key]['text_question'], type=questions[key]['question_type'], version= 1, form_question_id = questions[key]['form_question_id']))
+                session.commit()
+                continue
+            if (result.text_question != questions[key]['text_question'] or result.type != questions[key]['question_type']):
+                questions[key]['version'] = result.version + 1
+                session.add(Question(unique_id = questions[key]['unique_id'], form_id = questions[key]['form_id'], \
+                    text_question=questions[key]['text_question'], type=questions[key]['question_type'], version=questions[key]['version'], form_question_id = questions[key]['form_question_id']))
+                session.commit()
+            else:
+                for answer in answers:
+                    answer.update((k, result.unique_id) for k, v in answer.items() if v == questions[key]['unique_id'])
 
-            for key in range(len(answers)):
-                cursor.execute(f"SELECT text_answer, version FROM GoogleFormsAnswerType WHERE unique_question_id = '{answers[key]['unique_question_id']}' \
-                                AND response_id = '{answers[key]['response_id']}' ORDER BY version DESC")
-                result = cursor.fetchone()
-                if result is None:
-                    answers[key]['version'] = 1
-                    cursor.execute(f"INSERT INTO GoogleFormsAnswerType VALUES ('{answers[key]['unique_question_id']}', \
-                        '{answers[key]['response_id']}', %s, {answers[key]['timestamp']}, {answers[key]['version']})", (answers[key]['text_answer']))
-                    connection.commit()
-                    continue
-                if result['text_answer'] != answers[key]['text_answer']:
-                    answers[key]['version'] = result['version'] + 1
-                    cursor.execute(f"INSERT INTO GoogleFormsAnswerType VALUES ('{answers[key]['unique_question_id']}', \
-                        '{answers[key]['response_id']}', %s, {answers[key]['timestamp']}, {answers[key]['version']})", (answers[key]['text_answer']))
-                    connection.commit()
+        for key in range(len(answers)):
+            result = session.query(Answer).filter(Answer.unique_question_id == answers[key]['unique_question_id'], \
+                Answer.response_id == answers[key]['response_id']).order_by(Answer.version.desc()).first()
+            if result is None:
+                answers[key]['version'] = 1
+                session.add(Answer(unique_question_id = answers[key]['unique_question_id'], response_id = answers[key]['response_id'], \
+                    text_answer=answers[key]['text_answer'], timestamp=answers[key]['timestamp'], version=answers[key]['version']))
+                session.commit()
+                continue
+            if result.text_answer != answers[key]['text_answer']:
+                answers[key]['version'] = result.version + 1
+                session.add(Answer(unique_question_id = answers[key]['unique_question_id'], response_id = answers[key]['response_id'], \
+                    text_answer=answers[key]['text_answer'], timestamp=answers[key]['timestamp'], version=answers[key]['version']))
+                session.commit()
     finally:
-        connection.close()
+        session.close()
 
 main()

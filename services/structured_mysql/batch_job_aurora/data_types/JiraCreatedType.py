1bee039eef9baa704c25842b0750c4228944eb4a
Aksel Karlsen <aksel.karlsen@knowit.no>
2020-01-15 09:55:30 +0100

    deleted unused data types for jira

--- services/structured_mysql/batch_job_aurora/data_types/JiraCreatedType.py
+++
@@ -1,9 +0,0 @@
-from data_types.AbstractType import AbstractType
-
-
-class JiraCreatedType(AbstractType):
-    attributes_keep = {
-        ("issue", str): ["data", "issue"],
-        ("customer", str): ["data", "customer"],
-        ("created", str): ["data", "created"]
-    }
--- services/structured_mysql/batch_job_aurora/data_types/JiraUpdatedType.py
+++
@@ -1,9 +0,0 @@
-from data_types.AbstractType import AbstractType
-
-
-class JiraUpdatedType(AbstractType):
-    attributes_keep = {
-        ("issue", str): ["data", "issue"],
-        ("status", str): ["data", "status"],
-        ("updated", str): ["data", "updated"]
-    }

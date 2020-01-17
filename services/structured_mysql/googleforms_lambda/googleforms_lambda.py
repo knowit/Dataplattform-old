3d21d601abcbb41476b3b55b24f25bbc42fca352
Kim Hovden Duong <kimhduo@gmail.com>
2020-01-17 16:17:18 +0100

    Added support for choosing which questions in a form to include in the dataplatform.

--- services/structured_mysql/googleforms_lambda/googleforms_lambda.py
+++ services/structured_mysql/googleforms_lambda/googleforms_lambda.py
@@ -14,6 +14,7 @@ def split_and_sort_records(docs):
     questions = []
     answers = []
     records = sorted(docs, key = lambda i: i['responseTimestamp'])
+
     for record in records:
         question_uuid = str(uuid.uuid4())
         answer_uuid = str(uuid.uuid4())
@@ -46,42 +47,77 @@ def insert_or_update_forms_data(forms, questions, answers):
     Question = Base.classes.GoogleFormsQuestionType
     Answer = Base.classes.GoogleFormsAnswerType
     session = Session(engine)
+
     try: 
         for key in list(forms):
             result = session.query(Form).filter(Form.id == key).first()
             if result is None:
-                session.add(Form(id=key, title=forms[key]['title'], description=forms[key]['description'], created_at=forms[key]['created_at'], \
-                    owner=forms[key]['owner'], published_url=forms[key]['published_url']))
+                session.add(Form(
+                    id=key, 
+                    title=forms[key]['title'], 
+                    description=forms[key]['description'], 
+                    created_at=forms[key]['created_at'], 
+                    owner=forms[key]['owner'], 
+                    published_url=forms[key]['published_url']))
+
         for key in range(len(questions)):
-            result = session.query(Question).filter(Question.form_question_id == questions[key]['form_question_id'], \
-                Question.form_id == questions[key]['form_id']).order_by(Question.version.desc()).first()
+            result = session.query(Question).filter(
+                    Question.form_question_id == questions[key]['form_question_id'],
+                    Question.form_id == questions[key]['form_id']).order_by(Question.version.desc()).first()
             if result is None:
-                session.add(Question(unique_id = questions[key]['unique_id'], form_id = questions[key]['form_id'], \
-                    text_question=questions[key]['text_question'], type=questions[key]['question_type'], version= 1, form_question_id = questions[key]['form_question_id']))
+                session.add(Question(
+                    unique_id = questions[key]['unique_id'], 
+                    form_id = questions[key]['form_id'], 
+                    text_question=questions[key]['text_question'], 
+                    type=questions[key]['question_type'], 
+                    version= 1, 
+                    form_question_id = questions[key]['form_question_id']))
                 session.commit()
                 continue
+
             if (result.text_question != questions[key]['text_question'] or result.type != questions[key]['question_type']):
                 questions[key]['version'] = result.version + 1
-                session.add(Question(unique_id = questions[key]['unique_id'], form_id = questions[key]['form_id'], text_question=questions[key]['text_question'], \
-                    type=questions[key]['question_type'], version=questions[key]['version'], form_question_id = questions[key]['form_question_id']))
+                session.add(Question(
+                    unique_id = questions[key]['unique_id'], 
+                    form_id = questions[key]['form_id'], 
+                    text_question=questions[key]['text_question'], \
+                    type=questions[key]['question_type'], 
+                    version=questions[key]['version'], 
+                    form_question_id = questions[key]['form_question_id']))
                 session.commit()
             else:
                 for answer in answers:
                     answer.update((k, result.unique_id) for k, v in answer.items() if v == questions[key]['unique_id'])
+
         for key in range(len(answers)):
-            result = session.query(Answer).filter(Answer.unique_question_id == answers[key]['unique_question_id'], \
+            result = session.query(Answer).filter(
+                Answer.unique_question_id == answers[key]['unique_question_id'],
                 Answer.response_id == answers[key]['response_id']).order_by(Answer.version.desc()).first()
             if result is None:
                 answers[key]['version'] = 1
-                session.add(Answer(unique_id = answers[key]['unique_id'], unique_question_id = answers[key]['unique_question_id'], response_id = answers[key]['response_id'], \
-                    text_answer=answers[key]['text_answer'], timestamp=answers[key]['timestamp'], version=answers[key]['version'], form_id=answers[key]['form_id']))
+                session.add(Answer(
+                    unique_id = answers[key]['unique_id'], 
+                    unique_question_id = answers[key]['unique_question_id'], 
+                    response_id = answers[key]['response_id'], \
+                    text_answer=answers[key]['text_answer'], 
+                    timestamp=answers[key]['timestamp'], 
+                    version=answers[key]['version'], 
+                    form_id=answers[key]['form_id']))
                 session.commit()
                 continue
+
             if result.text_answer != answers[key]['text_answer']:
                 answers[key]['version'] = result.version + 1
-                session.add(Answer(unique_id = answers[key]['unique_id'], unique_question_id = answers[key]['unique_question_id'], response_id = answers[key]['response_id'], \
-                    text_answer=answers[key]['text_answer'], timestamp=answers[key]['timestamp'], version=answers[key]['version'], form_id=answers[key]['form_id']))
+                session.add(Answer(
+                    unique_id = answers[key]['unique_id'], 
+                    unique_question_id = answers[key]['unique_question_id'], 
+                    response_id = answers[key]['response_id'], \
+                    text_answer=answers[key]['text_answer'], 
+                    timestamp=answers[key]['timestamp'], 
+                    version=answers[key]['version'], 
+                    form_id=answers[key]['form_id']))
                 session.commit()
+    
     finally:
         session.close()
     
@@ -96,3 +132,4 @@ def handler(event, context):
             'statusCode': 200,
             'body': 'Done'
     }
+    

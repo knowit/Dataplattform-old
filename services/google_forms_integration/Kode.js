aa4b25cb5299ed3c8bbaa8634e24900824cf5bd6
Kim Hovden Duong <kimhduo@gmail.com>
2020-02-10 14:08:19 +0100

    Fixed bugs and added support for adding and removing all data from form

--- services/google_forms_integration/Kode.js
+++ services/google_forms_integration/Kode.js
@@ -1,31 +1,3 @@
-var SPREADSHEET = PropertiesService.getScriptProperties().getProperty('spreadsheet');
-var DATAPLATTFORM_INGEST_APIKEY = PropertiesService.getScriptProperties().getProperty('dataplattform_ingest_apikey');
-var DATAPLATTFORM_INGEST_URL = PropertiesService.getScriptProperties().getProperty('dataplattform_ingest_url');
-
-function doGet(e) {
-  var formId = e['parameter']['formId'];
-  var action = e['parameter']['action'];
-  switch (action) {
-    case 'add':
-      var isValidForm = getAccessToForm(formId);
-      if (!isValidForm) {
-        return HtmlService.createHtmlOutput('We could not find your form :( Please check that the Id is correct and that it is not a quiz!');
-      }
-      if (formExists(SPREADSHEET, formId)) {
-        return HtmlService.createHtmlOutput('Your form is already in the platform and can\'t be added again!');
-      }
-      writeToSpreadsheet(SPREADSHEET, formId);
-      return HtmlService.createHtmlOutput('Your form has been added to the dataplatform and will now be polled for new responses indefintely :)!');
-    case 'remove':
-      if (removeForm(SPREADSHEET, formId)) {
-        return HtmlService.createHtmlOutput('Responses will no longer be collected from your form!');
-      }
-      return HtmlService.createHtmlOutput('Could not find your form, are you sure it\'s been added to the dataplatform already? :)');
-    default:
-      return HtmlService.createHtmlOutput('That\'s not a valid action! Valid actions are \'add\' or \'remove\'');
-  }
-}
-
 function onInstall(e) {
   onOpen(e);
 }
@@ -36,48 +8,94 @@ function onOpen(e) {
     .createMenu('Dataplattform')
     .addItem('Start collecting data from this form', 'showQuestionsToInclude')
     .addItem('Stop collecting data from this form', 'stopCollectingData')
+    .addItem('Send all data from this form', 'sendAllData')
+    .addItem('Delete data related to this form from the platform', 'removeAllData')
     .addToUi();
 }
 
+function removeAllData() {
+  var scriptProperties = PropertiesService.getScriptProperties()
+  var apiKey = scriptProperties.getProperty('dataplattform_ingest_apikey');
+  var url = scriptProperties.getProperty('dataplattform_ingest_url');
+  var formId = FormApp.getActiveForm().getId();
+  data = [{
+    'deletionProcedure': true, 
+    'formId': formId
+  }];
+  var options = {
+    'method': 'post',
+    'contentType': 'application/json',
+    'payload': JSON.stringify(data),
+    'headers': { 'x-api-key': apiKey }
+  };
+  UrlFetchApp.fetch(url, options);
+
+  var htmlOutput = HtmlService
+    .createHtmlOutput("All data related to your form has been removed from the dataplatform! Remember to also stop collection of new data if relevant!")
+    .setWidth(250)
+    .setHeight(300);
+  FormApp.getUi().showModalDialog(htmlOutput, 'Result');
+}
+
 function showQuestionsToInclude() {
-  var html = HtmlService.createTemplateFromFile('Filters').evaluate();
+  var html = HtmlService.createTemplateFromFile('start_collection').evaluate();
   FormApp.getUi().showModalDialog(html, 'Start collecting data to the dataplatform.');
+}
+
+function sendAllData() {
+  var html = HtmlService.createTemplateFromFile('send_all_data').evaluate();
+  FormApp.getUi().showModalDialog(html, 'Send all your data');
 }
 
 function getFormQuestions() {
   return FormApp.getActiveForm().getItems()
 }
 
-function processForm(formObject) {
-  var questionsToInclude = [];
-  for (var key in formObject) {
-    questionsToInclude.push(formObject[key]);
+function processCollectionForm(formObject) {
+  var questionsToInclude = formObject["questions"]
+  Logger.log(formObject);
+  Logger.log(questionsToInclude);
+  if (questionsToInclude.length < 1) {
+    return "You didn't select any questions!"
   }
   result = addToPlatform(questionsToInclude)
   return result;
 }
 
+function processAllDataForm(formObject) {
+  var scriptProperties = PropertiesService.getScriptProperties()
+  var formId = FormApp.getActiveForm().getId();
+  var questionsToInclude = formObject["questions"]
+  var timestamp = 0
+  if (formObject['time'].length > 0) {
+    timestamp = new Date(formObject['time'])
+  }
+  postFormDataToIngest(scriptProperties, formId, timestamp, questionsToInclude);
+  return "All your data was added! Want to collect new data automatically? Turn on collection of data from this form as well!"
+}
+
 function addToPlatform(questionsToInclude) {
+  var spreadsheetId = PropertiesService.getScriptProperties().getProperty('spreadsheet_Id');
   var formId = FormApp.getActiveForm().getId();
 
-  var isValidForm = getAccessToForm(formId);
-  if (!isValidForm) {
-    var output = 'We could not find your form :( Please check that the Id is correct and that it is not a quiz!';
+  if (FormApp.getActiveForm().isQuiz()) {
+    var output = 'Your form is a quiz and cannot be added!';
   }
-  else if (formExists(SPREADSHEET, formId)) {
-    var output = 'Your form is already in the platform and can\'t be added again!';
+  else if (formExists(spreadsheetId, formId)) {
+    var output = 'Your form is already in the platform and can\'t be added again! If you want to change questions being collected, remove and add the form again.';
   }
   else {
-    writeToSpreadsheet(SPREADSHEET, formId, questionsToInclude);
+    writeToSpreadsheet(spreadsheetId, formId, questionsToInclude);
     var output = 'Your form has been added to the dataplatform and will now be polled for new responses indefintely :)!';
   }
   return output;
 }
 
 function stopCollectingData() {
+  var spreadsheetId = PropertiesService.getScriptProperties().getProperty('spreadsheet_Id');
   var formId = FormApp.getActiveForm().getId();
 
-  if (removeForm(SPREADSHEET, formId)) {
+  if (removeForm(spreadsheetId, formId)) {
     var output = 'Responses will no longer be collected from your form!';
   }
   else {
@@ -91,17 +109,25 @@ function stopCollectingData() {
   FormApp.getUi().showModalDialog(htmlOutput, 'Result');
 }
 
-function getAccessToForm(formId) {
-  try {
-    var form = FormApp.openById(formId);
-    if (form.isQuiz()) {
-      return false;
-    }
+function removeForm(spreadsheetId, formId) {
+  var row = formExists(spreadsheetId, formId)
+  if (row != false) {
+    Sheets.Spreadsheets.batchUpdate({
+      "requests":
+        [{
+          "deleteDimension": {
+            "range": {
+              "dimension": "ROWS",
+              "startIndex": row,
+              "endIndex": row+1
+            }
+          }
+        },
+        ],
+    }, spreadsheetId);
     return true;
   }
-  catch (e) {
-    return false;
-  }
+  return false;
 }
 
 function formExists(spreadsheetId, formId) {
@@ -123,41 +149,22 @@ function writeToSpreadsheet(spreadsheetId, formId, questionsToInclude) {
   var appendRequest = Sheets.newAppendCellsRequest();
   appendRequest.sheetId = spreadsheetId;
   appendRequest.rows = [valueRange];
-  var result = Sheets.Spreadsheets.Values.append(valueRange, spreadsheetId, 'A:C', {
+  Sheets.Spreadsheets.Values.append(valueRange, spreadsheetId, 'A:C', {
     valueInputOption: 'RAW'
   });
 }
 
-function removeForm(spreadsheetId, formId) {
-  // Don't allow removal of a form the user does not have access to
-  if (formExists(spreadsheetId, formId) && getAccessToForm(formId)) {
-    Sheets.Spreadsheets.batchUpdate({
-      "requests":
-        [{
-          "deleteDimension": {
-            "range": {
-              "dimension": "ROWS",
-              "startIndex": row,
-              "endIndex": row + 1
-            }
-          }
-        },
-        ],
-    }, spreadsheetId);
-    return true;
-  }
-  return false;
-}
-
 function runAtInterval() {
-  readDataFromSpreadsheet(SPREADSHEET)
+  var scriptProperties = PropertiesService.getScriptProperties()
+  readDataFromSpreadsheet(scriptProperties) 
+  
 }
 
-function readDataFromSpreadsheet(spreadsheetId) {
+function readDataFromSpreadsheet(scriptProperties) {
+  var spreadsheetId = scriptProperties.getProperty('spreadsheet_Id')
   var response = Sheets.Spreadsheets.Values.get(spreadsheetId, 'A:C');
   var formId;
   var timestamp;
-  var questions;
   for (row in response.values) {
     row = parseInt(row);
     // Skip headers
@@ -167,46 +174,60 @@ function readDataFromSpreadsheet(spreadsheetId) {
     formId = response.values[row][0];
     timestamp = new Date(response.values[row][1]);
     questionsToInclude = response.values[row][2].split(",");
-    postFormDataToIngest(formId, timestamp, questionsToInclude, row + 1);
+    postFormDataToIngest(scriptProperties, formId, timestamp, questionsToInclude);
+    updateSpreadsheetTimestamp(spreadsheetId, row+1, new Date());
   }
 }
 
-function postFormDataToIngest(formId, timestamp, questionsToInclude, rowToUpdate) {
-  var file = DriveApp.getFileById(formId);
-  var form = FormApp.openById(formId);
-  var formResponses = form.getResponses(timestamp);
-  updateSpreadsheetTimestamp(SPREADSHEET, rowToUpdate, new Date());
+function postFormDataToIngest(scriptProperties, formId, timestamp, questionsToInclude) {
+  var apiKey = scriptProperties.getProperty('dataplattform_ingest_apikey');
+  var url = scriptProperties.getProperty('dataplattform_ingest_url');
+  try {
+    var file = DriveApp.getFileById(formId);
+    var form = FormApp.openById(formId);    
   
-  for (var i = 0; i < formResponses.length; i++) {
-    var formResponse = formResponses[i];
-    var itemResponses = formResponse.getItemResponses();
-    for (var j = 0; j < itemResponses.length; j++) {
-      var itemResponse = itemResponses[j];
-      if (questionsToInclude.indexOf(itemResponse.getItem().getId().toString()) > -1) {
-        var data = {
-          'formId': form.getId(),
-          'formTitle': form.getTitle(),
-          'formDescription': form.getDescription(),
-          'formCreated': Math.floor(file.getDateCreated().valueOf() / 1000),
-          'formOwner': file.getOwner().getName(),
-          'formPublishedUrl': form.getPublishedUrl(),
-          'responseId': formResponse.getId(),
-          'responseTimestamp': Math.floor(formResponse.getTimestamp().valueOf() / 1000),
-          'responseQuestion': itemResponse.getItem().getTitle(),
-          'responseAnswer': itemResponse.getResponse(),
-          'questionId': itemResponse.getItem().getId(),
-          'questionType': itemResponse.getItem().getType().toString()
-        };
-        data = [data];
-        var options = {
-          'method': 'post',
-          'contentType': 'application/json',
-          'payload': JSON.stringify(data),
-          'headers': { 'x-api-key': DATAPLATTFORM_INGEST_APIKEY }
-        };
-        UrlFetchApp.fetch(DATAPLATTFORM_INGEST_URL, options);
+    if (timestamp == 0) {
+      var formResponses = form.getResponses();
+    }
+    else {
+      var formResponses = form.getResponses(timestamp);
+    }
+    for (var i = 0; i < formResponses.length; i++) {
+      var formResponse = formResponses[i];
+      var itemResponses = formResponse.getItemResponses();
+      for (var j = 0; j < itemResponses.length; j++) {
+        var itemResponse = itemResponses[j];
+        if (questionsToInclude.indexOf(itemResponse.getItem().getId().toString()) > -1) {
+          var data = {
+            'formId': form.getId(),
+            'formTitle': form.getTitle(),
+            'formDescription': form.getDescription(),
+            'formCreated': Math.floor(file.getDateCreated().valueOf() / 1000),
+            'formOwner': file.getOwner().getName(),
+            'formPublishedUrl': form.getPublishedUrl(),
+            'responseId': formResponse.getId(),
+            'responseTimestamp': Math.floor(formResponse.getTimestamp().valueOf() / 1000),
+            'responseQuestion': itemResponse.getItem().getTitle(),
+            'responseAnswer': itemResponse.getResponse(),
+            'questionId': itemResponse.getItem().getId(),
+            'questionType': itemResponse.getItem().getType().toString()
+          };
+          data = [data];
+          var options = {
+            'method': 'post',
+            'contentType': 'application/json',
+            'payload': JSON.stringify(data),
+            'headers': { 'x-api-key': apiKey }
+          };
+          UrlFetchApp.fetch(url, options);
+        }
       }
     }
+  }
+  catch (e) {
+    console.error(e.name)
+    console.error(e.name + ": " + e.message)
+    console.error("formId: " + formId)
   }
 }
 
@@ -217,7 +238,7 @@ function updateSpreadsheetTimestamp(spreadsheetId, row, timestamp) {
   var valueRange = Sheets.newValueRange();
   valueRange.values = values;
   var range = "B" + row;
-  var result = Sheets.Spreadsheets.Values.update(valueRange, spreadsheetId, range, {
+  Sheets.Spreadsheets.Values.update(valueRange, spreadsheetId, range, {
     valueInputOption: 'RAW'
   });
 }
